cd9f5b46352d7d20ca10d36abf00a727466a4e4f
Geri Coleman <gericoleman36@gmail.com>
2025-09-21 15:25:24 -0400

    Update __init__.py

--- BotBackup/__init__.py
+++ BotBackup/__init__.py
@@ -3,5 +3,5 @@ from .botbackup import BotBackup
 __red_end_user_data_statement__ = "This cog stores backup files of bot configurations. These backups may contain server settings and user data from other cogs."
 
 
-def setup(bot):
-    bot.add_cog(BotBackup(bot))
+async def setup(bot):
+    await bot.add_cog(BotBackup(bot))
